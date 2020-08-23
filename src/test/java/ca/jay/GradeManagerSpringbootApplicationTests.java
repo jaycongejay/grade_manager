3c37729d66dd61b5c8eac0aa2d70db7993634802
Jaehyung Lee <jhcongejh@gmail.com>
2020-08-22 20:02:28 -0400

    initial commit

---
+++ src/test/java/ca/jay/GradeManagerSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.jay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeManagerSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
